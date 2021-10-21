912c990be680f614cff52965fe2d3de1642c43be
sangramsahutech <64765045+sangramsahutech@users.noreply.github.com>
2021-10-22 01:00:56 +0800

    Ques2
    Added logic to check whether the input tree is BST or not. If not convert to BST.

--- src/com/gl/assignment/driver/BST2SkewedTreeConversion.java
+++ src/com/gl/assignment/driver/BST2SkewedTreeConversion.java
@@ -1,15 +1,17 @@
 package com.gl.assignment.driver;
+import com.gl.assignment.BSTutility.BSTUtil;
 /*
  * 
- * Logic -  1. Insert elements into the BST.
- * 			2. Recursively do inorder traversal and then push the data item into the new skewed tree by making its left child null
- * 			3. I am keeping the reference of root in a prev node so as to insert the next element into the right child of prev node.
- * 			4. Finally print the skewed tree by reading the right child of every node starting from head node recursively.
+ * Logic -  1. Insert elements into the binary tree.
+ *	    2. Check whether the binary tree is BST or not. If not convert to BST.
+ * 	    3. Recursively do inorder traversal and then push the data item into the new skewed tree by making its left child null
+ * 	    4. I am keeping the reference of root in a prev node so as to insert the next element into the right child of prev node.
+ * 	    5. Finally print the skewed tree by reading the right child of every node starting from head node recursively.
  */
 public class BST2SkewedTreeConversion {
-	static class Node{
-		int data;
-		Node left, right;
+	public static class Node{
+		public int data;
+		public Node left, right;
 		Node(int data){
 			this.data=data;
 			this.left=this.right=null;
@@ -21,11 +23,15 @@ public class BST2SkewedTreeConversion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BST2SkewedTreeConversion tree = new BST2SkewedTreeConversion();
+		BSTUtil bstObj = new BSTUtil();
 		tree.node = new Node(50);
 		tree.node.left=new Node(30);
 		tree.node.right = new Node(60);
 		tree.node.left.left = new Node(10);
-		tree.node.left.right = new Node(40);
+		tree.node.right.left = new Node(40);
+		boolean isBST = bstObj.chkBST(node,null,null);
+		if(!isBST)
+			bstObj.convBST(node);
 		inorderTrav(node);
 		getSkewedTree(head);
 	}
